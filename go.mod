11881bc75deef7189f05f78b3794d0a91a408732
Saif <droconos99@gmail.com>
2024-12-27 11:42:20 +0600

    Add: Logger, Driver and Read, Write, New, Delete functions.

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/saif-gitreps/go-nosql-database
 
 go 1.22.1
+
+require (
+	github.com/blend/go-sdk v1.20240719.1 // indirect
+	github.com/jcelliott/lumber v0.0.0-20160324203708-dd349441af25 // indirect
+)
--- main.go
+++ main.go
@@ -3,7 +3,187 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"github.com/jcelliott/lumber"
 )
+
+const Version = "1.0.1"
+
+type (
+	Logger interface {
+		Fatal(string, ...interface{})
+		Error(string, ...interface{})
+		Warn(string, ...interface{})
+		Info(string, ...interface{})
+		Trace(string, ...interface{})
+		Debug(string, ...interface{})
+	}
+
+	Driver struct {
+		mutex sync.Mutex
+		mutexes map[string]*sync.Mutex
+		dir string
+		log Logger
+	}
+)
+
+type Options struct {
+	Logger 
+}
+
+func New(dir string, options *Options)(*Driver, error) {
+	dir = filepath.Clean(dir)
+
+	opts := Options{}
+
+	if options != nil {
+		opts = *options
+	}
+
+	if opts.Logger == nil {
+		opts.Logger = lumber.NewConsoleLogger(lumber.INFO)
+	}
+
+	driver := Driver {
+		dir: dir,
+		mutexes: make(map[string]*sync.Mutex),
+		log: opts.Logger,
+	}
+
+	if _, err := os.Stat(dir); err == nil {
+		opts.Logger.Debug("Using '%s' (database already exists)\n", dir)
+		return &driver, nil
+	}
+
+	opts.Logger.Debug("Creating the database at '%s ...'\n", dir)
+
+	return &driver, os.Mkdir(dir, 0755)
+}
+
+func (d *Driver) Write(collection, resource string, v interface{}) error {
+	if collection == "" {
+		return fmt.Errorf("collection cannot be empty")
+	}
+
+	if resource == ""{
+		return fmt.Errorf("resource cannot be empty")
+	}
+
+	mutex := d.getOrCreateMutex(collection)
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	dir := filepath.Join(d.dir, collection)
+
+	finalPath := filepath.Join(dir, resource + ".json")
+
+	tempPath := finalPath + ".tmp"
+	
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
+	}
+
+	b, err := json.MarshalIndent(v, "", "\t")
+
+	if err != nil {
+		return err
+	}
+
+	b = append(b, byte('\n'))
+
+	if err := ioutil.WriteFile(tempPath, b, 0644); err != nil {
+		return err
+	}
+
+	return os.Rename(tempPath, finalPath)
+}
+
+func (d *Driver) Read(collection, resource string, v interface{}) error {
+	if collection == "" {
+		return fmt.Errorf("collection cannot be empty")
+	}
+	
+	if resource == "" {
+		return fmt.Errorf("resource cannot be empty")
+	}
+
+	record := filepath.Join(d.dir, collection, resource)
+
+	if _, err := stat(record); err != nil {
+		return err
+	}
+
+	b, err := ioutil.ReadFile(record + ".json")
+
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(b, v)
+}
+
+func (d *Driver) ReadAll(collection string)([]string, error) {
+	if collection == "" {
+		return nil, fmt.Errorf("collection cannot be empty")
+	}
+
+	dir := filepath.Join(d.dir, collection)
+
+	files, err := ioutil.ReadDir(dir)
+
+	if err != nil {
+		return nil, err
+	}
+
+	records := []string{}
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		b, err := ioutil.ReadFile(filepath.Join(dir, file.Name()))
+
+		if err != nil {
+			return nil, err
+		}
+
+		records = append(records, string(b))
+	}
+
+	return records, nil
+}
+
+func (d *Driver) Delete() error {
+	
+}
+
+func (d *Driver) getOrCreateMutex(collection string) *sync.Mutex {
+	m, ok := d.mutexes[collection]
+
+	d.mutex.Lock()
+	defer d.mutex.Unlock()
+
+	if !ok {
+		m = &sync.Mutex{} // create an empty mutex
+		d.mutexes[collection] = m
+	}
+
+	return m
+}
+
+func stat(path string)(fi os.FileInfo, err error) {
+	if fi, err = os.Stat(path); os.IsNotExist(err) {
+		fi, err = os.Stat(path + ".json") // dbs we create will have .json extension
+	}
+
+	return 
+}
+
 
 type Address struct {
 	City string
@@ -73,4 +253,12 @@ func main() {
 	}
 
 	fmt.Println("All Users", allUsers)
+
+	// if err := db.Delete("user", "John"); err != nil {
+	// 	fmt.Println("Error", err)
+	// }
+
+	// if err := db.Delete("user", ""); err != nil {
+	// 	fmt.Println("Error", err)
+	// }
 }
